386e30ad6467a5c1c1456d9685b9bae8298ca030
Vladislav Myzov <mozov.vlad@mail.ru>
2021-09-09 16:37:00 +0300

    First commit

---
+++ src/firsthomework/FirstHomework.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthomework;
+
+import static java.lang.String.format;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Влад
+ */
+public class FirstHomework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите градусы по Цельсию: ");
+        float TC = scanner.nextFloat();
+        float TF = (TC * 9/5) + 32;
+        System.out.println("Градусы по Фаренгейту: "+TF);
+    }
+    
+}
