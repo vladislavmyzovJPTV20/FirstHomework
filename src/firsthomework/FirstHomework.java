571824adfcaf429c7fc28f491f0cf6fd2fa005fc
Vladislav Myzov <mozov.vlad@mail.ru>
2021-09-28 21:00:09 +0300

    Нашли градусы по Фаренгейту, получив значение по Цельсию

--- src/firsthomework/FirstHomework.java
+++ src/firsthomework/FirstHomework.java
@@ -18,6 +18,7 @@ public class FirstHomework {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Создали сканер для воода градусов по Цельсию и вычислили градусы по Фаренгейту
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите градусы по Цельсию: ");
         float TC = scanner.nextFloat();
